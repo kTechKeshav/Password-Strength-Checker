f86b15678bdf1cc64de7470baa504b1a69cb5477
Keshav Awasthi <147409782+kTechKeshav@users.noreply.github.com>
2024-05-04 23:08:47 +0530

    All files uploaded successfully.

---
+++ script.js
@@ -0,0 +1,58 @@
+const inputfield = document.querySelector("#passwordfield");
+const outputfield = document.querySelector("#output");
+const showbtn = document.querySelector("#showpassword");
+const clr = document.querySelector("#clrbtn");
+
+function passwordchecker(inputfield) {
+  let password = inputfield.value;
+
+  if (password.length < 8) {
+    outputfield.innerHTML = "Password is too short";
+    outputfield.style.color = "red";
+  } else {
+    if (password.search(/[a-z]/) == -1) {
+      outputfield.innerHTML = "Password must contain lowercase letter (a-z)";
+      outputfield.style.color = "red";
+    } else {
+      if (password.search(/[A-Z]/) == -1) {
+        outputfield.innerHTML = "Password must contain uppercase letter (A-Z)";
+        outputfield.style.color = "red";
+      } else {
+        if (password.search(/[0-9]/) == -1) {
+          outputfield.innerHTML = "Password must contain digits (0-9)";
+          outputfield.style.color = "red";
+        } else {
+          if (password.search(/[~\!\@\#\$\%\^\&\*\(\)\_\+]/) == -1) {
+            outputfield.innerHTML = "Password must contain special symbols";
+            outputfield.style.color = "red";
+          } else {
+            outputfield.innerHTML = "Your Password is Strong";
+            outputfield.style.color = "greenyellow";
+          }
+        }
+      }
+    }
+  }
+}
+
+inputfield.addEventListener("input", () => {
+  passwordchecker(inputfield);
+});
+
+let passwordflag = 0;
+showbtn.addEventListener("click", ()=>{
+      if(passwordflag == 0){
+            inputfield.type = "text";
+            showbtn.innerHTML = "Hide Password"
+            passwordflag = 1;
+      }else{
+            inputfield.type = "password"
+            showbtn.innerHTML = "Show Password"
+            passwordflag = 0;
+      }
+})
+
+clr.addEventListener("click",()=>{
+  inputfield.value = '';
+  outputfield.innerHTML = '';
+})
